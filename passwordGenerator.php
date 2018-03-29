06606a59051586497246ba5d3dbe8f67f61cf458
Greg Seymour <33111951+Lon-bon@users.noreply.github.com>
2018-03-29 15:26:29 -0600

    Update passwordGenerator.php

--- passwordGenerator.php
+++ passwordGenerator.php
@@ -2,8 +2,8 @@
 function generateRandomPassword() {
     $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!-()#~`.$";
     for ($i = 1; $i <= 16; $i++) {
-        $char = random_int(0, strlen($characters)-1);
-        $password .= $characters[$char];
+        $index = random_int(0, strlen($characters)-1);
+        $password .= $characters[$index];
     }
     echo $password;
 }
